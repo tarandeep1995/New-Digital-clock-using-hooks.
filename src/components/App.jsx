0e30c0c4600da959e34d11c243dcedfe91b4625a
tarandeep1995 <taran933@gmail.com>
2023-04-03 16:58:58 +0530

    Changes

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const timeOptions = {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: false
+  };
+  setInterval(updateTime, 1000);
+
+  const now = new Date().toLocaleTimeString("en-US", timeOptions);
+  const [time, setTime] = useState(now);
+
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString("en-US", timeOptions);
+    setTime(newTime);
+  }
+  return (
+    <div className="container">
+      <h2>CurrentTime</h2>
+      <h1>&nbsp;{time}</h1>
+      {/* <button onClick={updateTime}>Get Time</button> */}
+    </div>
+  );
+}
+export default App;
